81aae3210568378feda42b72cb10b67f77a2aac0
manjush.menon <manjushrmenon730@gmail.com>
2025-04-13 02:57:05 +0530

    added setup for fonts and tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { jetbrainsMono } from "@/utils/fonts";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">Default Font</h1>
+      <p className={`${jetbrainsMono.className} text-2xl text-green-400 mt-4`}>
+        This is Pacifico font — it should look cursive and playful!
+      </p>
+    </main>
+  );
 }
