7294809ef0475dafa7fb2f1bd8756ba6ac6001e2
manjush.menon <manjushrmenon730@gmail.com>
2025-04-13 11:53:48 +0530

    added tailwind config and set bg to black

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#000000",
+        foreground: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
